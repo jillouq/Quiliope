30ff5d72ef03184138ab5fef17e3e40584295701
unknown <jillouq@gmail.com>
2023-05-02 23:07:46 +0800

    May 2

---
+++ src/Interfaces/login.js
@@ -0,0 +1,19 @@
+import React, { Component } from 'react';
+import "./loginStyle.css";
+
+function Login() {
+    return (
+        <html>
+            <head></head>
+            <body>
+                <div class="tab">
+                    <a href='./Interfaces/inventory.js' target='_blank'>Inventory</a>
+                    <a>Medicine Lists</a>
+                    <a>Supplier</a>
+                </div>
+            </body>
+        </html>
+    );
+}
+ 
+export default Login;
--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import App from './App';
+//import App from './App';
 import Login from './Interfaces/login';
 import reportWebVitals from './reportWebVitals';
 
